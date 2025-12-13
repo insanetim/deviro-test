df08d78b0262e559ea437af0bcdd0f2695aa776f
insanetim <47120724+insanetim@users.noreply.github.com>
2025-12-13 12:55:33 +0200

    add mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import { CssBaseline } from "@mui/material"
+import { createTheme, ThemeProvider } from "@mui/material/styles"
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+})
+
 const App = () => {
   return (
-    <div>
-      <h1>Top Secret Project</h1>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <div>
+        <h1>Top Secret Project</h1>
+      </div>
+    </ThemeProvider>
   )
 }
 
